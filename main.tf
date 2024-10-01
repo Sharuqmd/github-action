338cf605ed77fd99993c62fbcf2c7181eecd8ea7
SHARUQ <142597507+Sharuqmd@users.noreply.github.com>
2024-10-01 17:45:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-  region = "ap-southeast-1"  
+  region = "ap-southeast-1" 
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
 
 resource "aws_s3_bucket" "my_bucket" {
