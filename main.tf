5398c6eca6963dc712d030bc37ec74dc688d5504
SHARUQ <142597507+Sharuqmd@users.noreply.github.com>
2024-10-01 15:33:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,8 @@
+provider "aws" {
+  region = "us-east-1"  # Set your AWS region
+}
 
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-simple-terraform-bucket"  # Bucket name (must be globally unique)
+  acl    = "private"  # Set bucket to private
+}
